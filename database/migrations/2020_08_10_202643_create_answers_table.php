08b098172383db7728dc69935d58ac6329da125e
Adrien <github@adrien.sent.com>
2020-08-11 12:25:29 +0200

    feature: basic Answer & Question models with seeding

---
+++ database/migrations/2020_08_10_202643_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->timestamps();
+
+            $table
+              ->foreignId('question_id')
+              ->contrained()
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    factory(App\Question::class, 50)->create()->each(function ($question) {
+      factory(App\Answer::class, rand(0, 10))->make()->each(function ($answer) use ($question) {
+        $question->answers()->save($answer);
+      });
+    });
+  }
 }
